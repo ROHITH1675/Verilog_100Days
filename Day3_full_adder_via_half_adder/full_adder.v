44ad2cd59150eedbc0cfc85c7aa0d7103925e915
ROHITH T J <rohithtj2005@gmail.com>
2026-03-26 19:35:34 +0530

    Add files via upload

---
+++ Day3_full_adder_via_half_adder/full_adder.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:02:01 03/26/2026 
+// Design Name: 
+// Module Name:    full_adder 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module full_adder(a_in,b_in,c_in,sum,carry
+    );
+	 input a_in,b_in,c_in;
+	 output sum,carry;
+	 wire w1,w2,w3;
+	 
+	 half_adder h1(.a_in(a_in),.b_in(b_in),.sum(w1),.carry(w2));
+	 half_adder h2(.a_in(w1),.b_in(c_in),.sum(sum),.carry(w3));
+	 assign carry = w2 | w3;
+	 
+	 
+
+
+endmodule
---
+++ Day3_full_adder_via_half_adder/full_adder_tb.v
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   19:11:54 03/26/2026
+// Design Name:   full_adder
+// Module Name:   /home/rohithtj/Verilog_100/full_adder_via_half_adder/full_adder_tb.v
+// Project Name:  full_adder_via_half_adder
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: full_adder
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module full_adder_tb;
+
+	// Inputs
+	reg a_in;
+	reg b_in;
+	reg c_in;
+
+	// Outputs
+	wire sum;
+	wire carry;
+	integer i;
+
+	// Instantiate the Unit Under Test (UUT)
+	full_adder uut (
+		.a_in(a_in), 
+		.b_in(b_in), 
+		.c_in(c_in), 
+		.sum(sum), 
+		.carry(carry)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a_in = 0;
+		b_in = 0;
+		c_in = 0;
+
+	end
+	
+	initial 
+	for(i=0;i<8;i=i+1)begin
+	{c_in,b_in,a_in} =i;
+	#10;
+	end
+	 
+	initial $monitor("Input c_in=%b b_in=%b a_in=%b sum=%b carry=%b",c_in,b_in,a_in,sum,carry);
+	initial #1000 $finish;
+      
+endmodule
+
